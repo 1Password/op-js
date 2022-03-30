bb3de2fea636852abe3f7fa633fb8aaa3c0cbac0
Jody Heavener <jody.heavener@agilebits.com>
2022-03-30 15:46:04 -0300

    Foundation setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('@jest/types/build/Config').DefaultOptions} */
+module.exports = {
+	roots: ["<rootDir>"],
+	testMatch: ["<rootDir>/src/**/?(*.)+(test).[tj]s"],
+	testEnvironment: "jest-environment-jsdom",
+	preset: "ts-jest/presets/js-with-ts",
+	transform: {
+		"^.+\\.(ts|js)?$": "ts-jest",
+	},
+	testPathIgnorePatterns: [
+		"<rootDir>/dist",
+		"<rootDir>/node_modules",
+	]
+};
