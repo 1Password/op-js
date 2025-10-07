719614055c06db918e4d25dea6a793fea8bf2ce0
Jody Heavener <jody.heavener@agilebits.com>
2025-10-07 12:03:39 -0400

    Remove integration tests

--- tests/version.test.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import { version } from "../src";
-
-describe("version", () => {
-	it("returns the version number", () => {
-		expect(version()).toMatchSchema(Joi.string().required());
-	});
-});
